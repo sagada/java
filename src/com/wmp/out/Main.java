d9d8ef606ad85a278b858dda68171bd875795ef9
a <wlatmdqja@naver.com>
2020-05-28 00:52:08 +0900

    Collections 메서드

--- src/com/wmp/out/Main.java
+++ src/com/wmp/out/Main.java
@@ -1,9 +1,6 @@
 package com.wmp.out;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
@@ -23,6 +20,23 @@ public class Main {
             System.out.println("이름 : " + e.getKey() + " 점수 : " + e.getValue());
         }
 
+        set = map.keySet();
+        System.out.println("참가자 명단 : " + set);
+
+        Collection values = map.values();
+        it = values.iterator();
+        int total = 0;
+
+        while(it.hasNext())
+        {
+            Integer i = (Integer)it.next();
+            total += i.intValue();
+        }
+
+        System.out.println("총점  : " + total);
+        System.out.println("평균  : " + total/set.size());
+        System.out.println("최고 점수 : " + Collections.max(values));
+        System.out.println("최저 점수 : " + Collections.min(values));
     }
 }
 
