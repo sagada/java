10bacc0ba0b1562fcc061268e2540b3c9e358e07
sagada <wlatmdqja@naver.com>
2020-06-05 00:49:02 +0900

    enum2

--- src/com/wmp/out/javas/EnumEx1.java
+++ src/com/wmp/out/javas/EnumEx1.java
@@ -1,6 +1,19 @@
 package com.wmp.out.javas;
 
-enum Direction {EAST, SOUTH, WEST, NORTH}
+enum Direction {
+    EAST(1,"동"),
+    SOUTH(5,"북"),
+    WEST(-1,"서"),
+    NORTH(10,"남");
+    private final int value;
+    private final String korean;
+    Direction(int value, String korean)
+    {
+        this.value = value;
+        this.korean = korean;
+    }
+    int getValue() {return value;}
+}
 
 public class EnumEx1 {
     public static void main(String[] args) {
@@ -18,5 +31,24 @@ public class EnumEx1 {
 //        System.out.println("d1 > d3 ?" + (d1> d3));
         System.out.println("d1.compareTo(d3) ? " + d1.compareTo(d2));
 
+        switch (d1)
+        {
+            case EAST:
+                System.out.println("EAST!~!");   break;
+            case SOUTH:
+                System.out.println("SOUTH!~!"); break;
+            case WEST:
+                System.out.println("WEST!~!");  break;
+            case NORTH:
+                System.out.println("NORTH!~!"); break;
+            default:
+                System.out.println("Invalid direction !");break;
+        }
+
+
+        Direction[] arr = Direction.values();
+
+        for(Direction d : arr)
+            System.out.printf("%s = %d%n", d.name(),d.ordinal());
     }
 }
