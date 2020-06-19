a4e43e6c9e0053a3e856de3beffbf4f8e571cbfc
sagada <wlatmdqja@naver.com>
2020-06-19 23:58:51 +0900

    stream 연습

--- src/com/wmp/out/stream/StreamTest.java
+++ src/com/wmp/out/stream/StreamTest.java
@@ -1,11 +1,27 @@
 package com.wmp.out.stream;
 
+
+import java.sql.Struct;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class StreamTest {
-    public static void main(String[] args) {
-        List<Student> s = new ArrayList<>();
 
+    public static void main(String[] args) {
+        List<Student> s = givenStudent();
+
+        List<String> names = s.stream()
+                .sorted(Comparator.comparing((Student::getName)).reversed())
+                .map(Student::getName)
+                .collect(Collectors.toList());
+
+        names.forEach(System.out::println);
+
+    }
+
+    static List<Student> givenStudent()
+    {
+        List<Student> s = new ArrayList<>();
         s.add(new Student("키워드1", 3, 300));
         s.add(new Student("키워드2", 3, 400));
         s.add(new Student("키워드3", 3, 300));
@@ -16,28 +32,7 @@ public class StreamTest {
         s.add(new Student("키워드4", 3, 300));
         s.add(new Student("키워드4", 3, 300));
 
-        Map<String, Integer> map = new HashMap<>();
-
-        s.forEach(e-> {
-            if (map.containsKey(e.getName())) {
-                map.put(e.getName(), 1);
-            } else {
-                map.put(e.getName(), map.get(e.getName()) + 1);
-            }
-        });
-
-        Set<Student> set = new HashSet<>();
-        Set<Student> set2 = new HashSet<>();
-
-        s.stream().filter(e->map.get(e.getName()) == 1).forEach(set::add);
-        s.stream().filter(e->map.get(e.getName()) != 1).forEach(set2::add);
-
-        set.forEach(e-> System.out.println(e.getName()));
-        System.out.println("중복");
-        set2.forEach(e-> System.out.println(e.getName()));
-//        map.forEach((name, count) -> {
-//            System.out.println(name + " / " + count);
-//        });
+        return s;
     }
 }
 
@@ -85,8 +80,4 @@ class Student  {
         this.totalScore = totalScore;
     }
 
-//    @Override
-//    public int compareTo(Student o) {
-//        return o.getName().compareToIgnoreCase(this.getName());
-//    }
 }
