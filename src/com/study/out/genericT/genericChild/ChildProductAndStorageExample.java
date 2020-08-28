fcb558f1df80504bd8492561ae4ca44433becb0f
ysh <wlatmdqja@naver.com>
2020-08-28 22:49:22 +0900

    stream

--- src/com/study/out/genericT/genericChild/ChildProductAndStorageExample.java
+++ src/com/study/out/genericT/genericChild/ChildProductAndStorageExample.java
@@ -2,9 +2,9 @@ package com.study.out.genericT.genericChild;
 
 public class ChildProductAndStorageExample {
     public static void main(String[] args) {
-        Storage<Tv> storage = new StorageImpl<Tv>(100);
-        storage.add(new Tv(), 0);
 
+
+        System.out.println(4/10.0);
 
     }
 }
---
+++ src/com/study/out/stree/Main.java
@@ -0,0 +1,4 @@
+package com.study.out.stree;
+
+public class Main {
+}
